0179b74403032fcfe80a0577848a6bc2a9911dbd
dhaulagiri0 <yiqinw4@gmail.com>
2020-06-09 21:41:12 +0800

    Updated text_parser to work

--- keras_frcnn/text_parser.py
+++ keras_frcnn/text_parser.py
@@ -39,12 +39,12 @@ def get_data(input_path):
                 all_imgs[filename]['width'] = cols
                 all_imgs[filename]['height'] = rows
                 all_imgs[filename]['bboxes'] = []
-                if np.random.randint(0,6) > 0:
-                    all_imgs[filename]['imageset'] = 'trainval'
-                else:
-                    all_imgs[filename]['imageset'] = 'test'
+#                 if np.random.randint(0,6) > 0:
+                all_imgs[filename]['imageset'] = 'train'
+#                 else:
+#                     all_imgs[filename]['imageset'] = 'test'
 
-            all_imgs[filename]['bboxes'].append({'class': class_name, 'x1': int(x1), 'x2': int(x2), 'y1': int(y1), 'y2': int(y2)})
+            all_imgs[filename]['bboxes'].append({'class': class_name, 'x1': int(float(x1)), 'x2': int(float(x2)), 'y1': int(float(y1)), 'y2': int(float(y2))})
 
 
         all_data = []
